5a3814023c419ec2a947fe54af03adf4410870be
Harry Whorlow <harry.whorlow@technoly.com>
2024-04-24 16:31:14 +0200

    files n shit

--- src/pages/Booking/index.tsx
+++ src/pages/Booking/index.tsx
@@ -1,5 +1,26 @@
-import { Container } from "@mui/material";
+import { Box, Grid, Toolbar, Typography } from "@mui/material";
+import { QRCode } from "react-qrcode-logo";
 
 export default function Booking() {
-  return <Container>heloods</Container>;
+  const [sessionId, setSessionId] = useState<string>("");
+
+  return (
+    <Box sx={{ height: "100vh", width: "100vh" }}>
+      <Toolbar sx={{ width: "100%" }}>
+        <Grid container width="100%">
+          <Grid item>
+            <Typography variant="h1" component="h1">
+              LEAVING EARLY
+            </Typography>
+          </Grid>
+        </Grid>
+      </Toolbar>
+
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item>
+          <QRCode value="scan me daddy"></QRCode>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
