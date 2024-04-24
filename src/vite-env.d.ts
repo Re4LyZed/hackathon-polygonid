5c11ba05aedb8899d8bf814fe4d7245cc81db0f5
Roman Gerber <roman.gerber@hotelplan.com>
2024-04-24 16:28:38 +0200

    polygonService init

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly POLYGON_API_URL: string;
+    readonly POLYGON_API_USER: string;
+    readonly POLYGON_API_PASSWORD: string;
+    // Declare other environment variables as needed
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
